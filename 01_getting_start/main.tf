57ff26fe9ce2f31e57aa40ac259c3946931992fa
trungdq-3037 <do.quang.trung@sun-asterisk.com>
2023-05-05 11:51:39 +0900

    tfvars

--- 01_getting_start/main.tf
+++ 01_getting_start/main.tf
@@ -9,14 +9,21 @@ terraform {
 
 provider "aws" {
   # Configuration options
-  profile = "trungadmin"
-  region  = "ap-northeast-1"
+    profile = "trungadmin"
+  region = "ap-northeast-1"
+}
+
+variable "instance_type" {
+  description = "type of ec2 instance"
+  type= string
+  default = "t2.micro"
 }
 
 
 resource "aws_instance" "created_by_terraform" {
   ami           = "ami-01b32aa8589df6208"
-  instance_type = "t2.micro"
+#   instance_type = "t2.micro"
+  instance_type = var.instance_type
 
   tags = {
     Name = "created_by_terraform"
