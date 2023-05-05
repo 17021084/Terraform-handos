4cc6780d7e99df997f927e5be9519073352fc5c3
trungdq-3037 <do.quang.trung@sun-asterisk.com>
2023-05-05 18:31:20 +0900

    terraform cloud

--- 01_getting_start/main.tf
+++ 01_getting_start/main.tf
@@ -1,3 +1,23 @@
+terraform {
+  # Nơi lưu trữ state của tf. remote day la terraform cloud
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "dotrunghg1999"
+    workspaces {
+      name = "terraform-getting-start"
+    }
+
+  }
+
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.65.0"
+    }
+  }
+}
+
 locals {
   project_name = "wakudoki"
 }
